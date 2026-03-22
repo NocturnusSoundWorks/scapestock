f05eceb4360b1d45cd5d965c1ed7350342a54aec
NocturnusSoundWorks <datgraphic@gmail.com>
2026-03-22 21:33:27 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,21 @@
+const PHOTOS = [
+
+{
+id:"25K062101",
+tags:["sky","cloud","sun","blue","nature","summer"],
+city:"tokyo"
+},
+
+{
+id:"25K062102",
+tags:["rain","street","night","city"],
+city:"tokyo"
+},
+
+{
+id:"25K062103",
+tags:["sakura","flower","spring","nature"],
+city:"tokyo"
+}
+
+]
