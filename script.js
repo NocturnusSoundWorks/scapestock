4919b438d2269d643c89c764f9ab147bff1b5486
NocturnusSoundWorks <datgraphic@gmail.com>
2026-03-22 22:06:16 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,77 @@
+function getPhotoId(){
+
+const params=new URLSearchParams(window.location.search)
+
+return params.get("id")
+
+}
+
+function loadGallery(){
+
+const gallery=document.getElementById("gallery")
+
+if(!gallery) return
+
+PHOTOS.forEach(photo=>{
+
+const link=document.createElement("a")
+
+link.href="photo.html?id="+photo.id
+
+const img=document.createElement("img")
+
+img.src="images/thu/"+photo.id+"_thu.jpg"
+
+img.alt=photo.tags.join(" ")
+
+img.loading="lazy"
+
+link.appendChild(img)
+
+gallery.appendChild(link)
+
+})
+
+}
+
+function loadPhotoPage(){
+
+const photoContainer=document.getElementById("photo-page")
+
+if(!photoContainer) return
+
+const id=getPhotoId()
+
+const photo=PHOTOS.find(p=>p.id===id)
+
+if(!photo) return
+
+const img=document.createElement("img")
+
+img.src="images/full/"+photo.id+".jpg"
+
+img.alt=photo.tags.join(" ")
+
+const download=document.createElement("a")
+
+download.href="images/full/"+photo.id+".jpg"
+
+download.className="download-btn"
+
+download.download=photo.id+".jpg"
+
+download.innerText="Download"
+
+photoContainer.appendChild(img)
+
+photoContainer.appendChild(download)
+
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+loadGallery()
+
+loadPhotoPage()
+
+})
