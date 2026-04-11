7d11de1a2b0911333af8e23224138414eaa32da5
NocturnusSoundWorks <datgraphic@gmail.com>
2026-04-11 21:40:52 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1142,8 +1142,8 @@ tags:["sakura","cherry-blossom","flower","spring","japan","pink","nature","bloss
 }, 
 {
 id:"26K032665",
-tags:["flower","sakura","cherry-blossom","leaf","scenery","pink","white","spring","nature","japan"]
-},
+tags:["sakura","cherry-blossom","flower","spring","japan","pink","nature","blossom","petal","tree","seasonal","outdoors","background","copy-space","wallpaper","water-droplet","after-rain","rain","no-people"]
+}, 
 {
 id:"26K032666",
 tags:["road","street","alley","bridge","red","downtown","japan","traditional"]
